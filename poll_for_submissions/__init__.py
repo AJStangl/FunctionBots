a50ad4d72ae82f90b8e2f1db6025cb20bf910ee2
AJStangl <ajstangl@gmail.com>
2022-05-08 08:44:44 -0400

    - Adding submission polling

--- poll_for_submissions/__init__.py
+++ poll_for_submissions/__init__.py
@@ -1,14 +1,39 @@
 import datetime
 import logging
-
+import asyncpraw
+import os
 import azure.functions as func
+from asyncpraw import Reddit
 
 
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+async def main(mytimer: func.TimerRequest) -> None:
+	utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+	# TODO: In order to support multiple bots we will need to change what is below into a loop
+	logging.info(f":: Poll For Submission trigger called at {utc_timestamp}")
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+	reddit = get_praw_instance("LarissaBot-GPT2")
+
+	user = await reddit.user.me()
+
+	logging.info(f":: Polling For Submissions for User {user.name}")
+
+	subs = get_subs_from_configuration()
+	print(subs)
+
+	await reddit.close()
+
+
+def get_praw_instance(bot_name: str) -> Reddit:
+	logging.info(f":: Initializing Reddit Praw Instance")
+	reddit = asyncpraw.Reddit(site_name=bot_name)
+	return reddit
+
+
+def get_recent_submissions(subreddit_name: str):
+	logging.info(f":: Obtaining New/Incoming Submissions For Subreddit {subreddit_name}")
+
+
+def get_subs_from_configuration() -> [str]:
+	subs = os.environ["SubReddit"].split(",")
+	return subs
