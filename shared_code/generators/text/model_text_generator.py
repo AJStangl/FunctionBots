9abb2c8d4b617407d8d501fa519cb49f89fbefed
AJStangl <ajstangl@gmail.com>
2022-05-15 14:37:20 -0400

    adding more

--- shared_code/generators/text/model_text_generator.py
+++ shared_code/generators/text/model_text_generator.py
@@ -6,7 +6,7 @@ from simpletransformers.language_generation import LanguageGenerationModel
 class ModelTextGenerator(object):
 	def __init__(self):
 		self.default_text_generation_parameters = {
-			'max_length': 1024,
+			'max_length': 512,
 			'num_return_sequences': 1,
 			'prompt': None,
 			'temperature': 0.8,
