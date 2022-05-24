bca5af8a66a8148649fe7fcbcbaca8f4fdf644d1
AJStangl <ajstangl@gmail.com>
2022-05-24 07:13:30 -0400

    Adding more Funtions

--- tag/__init__.py
+++ tag/__init__.py
@@ -1,7 +1,7 @@
 import json
 import logging
-from types import SimpleNamespace
 import datetime
+from typing import Optional
 import azure.functions as func
 
 from shared_code.helpers.reddit_helper import RedditHelper
@@ -10,7 +10,7 @@ from shared_code.helpers.tagging import Tagging
 
 def main(message: func.QueueMessage, promptMessage: func.Out[str]) -> None:
 
-	logging.info(f":: Trigger For Text Prompt Generation called at {datetime.date.today()}")
+	logging.info(f":: Trigger For Text Prompt Extraction called at {datetime.date.today()}")
 
 	message_json = message.get_body().decode('utf-8')
 
@@ -18,18 +18,22 @@ def main(message: func.QueueMessage, promptMessage: func.Out[str]) -> None:
 
 	incoming_message.text_generation_prompt = process_input(incoming_message)
 
-	promptMessage.set(incoming_message.json)
+	if incoming_message.text_generation_prompt is not None:
+		promptMessage.set(incoming_message.json)
 
-def process_input(incoming_message: TableRecord) -> str:
+def process_input(incoming_message: TableRecord) -> Optional[str]:
 	helper = RedditHelper()
 	instance = helper.get_praw_instance(incoming_message.responding_bot)
 
 	if incoming_message.input_type == "Submission":
 		thing = instance.submission(id=incoming_message.id)
 		history = Tagging.collate_tagged_comment_history(thing)
-	else:
+
+	if incoming_message.input_type == "Comment":
 		thing = instance.comment(id=incoming_message.id)
 		history = Tagging.collate_tagged_comment_history(thing)
+	else:
+		return None
 
 	cleaned_history = Tagging.remove_username_mentions_from_string(history, incoming_message.responding_bot)
 	reply_start_tag = Tagging.get_reply_tag(thing, incoming_message.responding_bot)
