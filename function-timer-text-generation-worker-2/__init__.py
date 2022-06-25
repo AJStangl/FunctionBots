4ea5ddc075ff8dac228c39df56980ba757458b8a
AJStangl <ajstangl@gmail.com>
2022-06-24 21:45:34 -0400

    It works better

--- function-timer-text-generation-worker-2/__init__.py
+++ function-timer-text-generation-worker-2/__init__.py
@@ -1,38 +1,17 @@
 import json
 import logging
-from datetime import datetime
 
 import azure.functions as func
 
-from shared_code.database.instance import TableRecord, TableHelper
 from shared_code.database.repository import DataRepository
 from shared_code.generators.text.model_text_generator import ModelTextGenerator
+from shared_code.services.text_generation import TextGenerationService
 
 
 def main(message: func.QueueMessage, responseMessage: func.Out[str]) -> None:
-
 	logging.debug(f":: Text Generation Timer Trigger Called")
-
+	model_generator: ModelTextGenerator = ModelTextGenerator()
 	repository: DataRepository = DataRepository()
-
-	incoming_message: TableRecord = TableHelper.handle_fucking_bullshit(message)
-
-	bot_name = incoming_message["RespondingBot"]
-
-	prompt = incoming_message["TextGenerationPrompt"]
-
-	logging.debug(f":: Trigger For Model Generation called at {datetime.now()} for {bot_name}")
-
-	model_generator = ModelTextGenerator()
-
-	result = model_generator.generate_text(bot_name, prompt)
-
-	entity = repository.get_entity_by_id(incoming_message["Id"])
-
-	entity.TextGenerationPrompt = prompt
-
-	entity.TextGenerationResponse = result
-
-	repository.update_entity(entity)
-
-	responseMessage.set(json.dumps(entity.as_dict()))
+	text_gen_service = TextGenerationService(repository, model_generator)
+	result = text_gen_service.invoke(message)
+	responseMessage.set(json.dumps(result))
---
+++ shared_code/services/text_generation.py
@@ -0,0 +1,40 @@
+import logging
+from datetime import datetime
+
+from shared_code.database.instance import TableRecord, TableHelper
+from shared_code.database.repository import DataRepository
+from shared_code.generators.text.model_text_generator import ModelTextGenerator
+
+
+class TextGenerationService:
+	def __init__(self, repository_instance: DataRepository, model_generator: ModelTextGenerator):
+		self.repository: DataRepository = repository_instance
+		self.model_generator: ModelTextGenerator = model_generator
+
+	def invoke(self, message) -> dict:
+
+		logging.debug(f":: Text Generation Timer Trigger Called")
+
+		incoming_message: TableRecord = TableHelper.handle_fucking_bullshit(message)
+
+		bot_name = incoming_message["RespondingBot"]
+
+		prompt = incoming_message["TextGenerationPrompt"]
+
+		logging.debug(f":: Trigger For Model Generation called at {datetime.now()} for {bot_name}")
+
+		result = self.model_generator.generate_text(bot_name, prompt)
+
+		entity = self.repository.get_entity_by_id(incoming_message["Id"])
+
+		if entity is None:
+			logging.info(f":: Entity Not Found For Id {incoming_message['Id']}")
+			return
+
+		entity.TextGenerationPrompt = prompt
+
+		entity.TextGenerationResponse = result
+
+		self.repository.update_entity(entity)
+
+		return entity.as_dict()
