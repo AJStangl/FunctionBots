80e55b9215f9731d46f681647b4033e9b1a68fdd
AJStangl <ajstangl@gmail.com>
2022-07-04 13:32:49 -0400

    added and changed ps1 scripts

---
+++ run_workers.ps1
@@ -0,0 +1,9 @@
+function RunBot
+{
+    conda activate reddit-function-bot
+    func start --functions function-timer-submit-post  function-queue-text-generation-worker-1 function-queue-text-generation-worker-2 --functions function-queue-text-generation-worker-3 --verbose -p 8001
+}
+
+while(1) {
+    RunBot
+}
