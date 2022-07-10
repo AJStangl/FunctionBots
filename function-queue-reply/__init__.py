2a1b93526d844798f28102d1739f667cb69e07c3
AJStangl <ajstangl@gmail.com>
2022-07-09 21:13:40 -0400

    Added a queue for replies

---
+++ function-queue-reply/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+import azure.functions as func
+
+from shared_code.services.reply_service import ReplyService
+
+
+async def main(message: func.QueueMessage) -> None:
+	logging.info(f":: Reply Service Invocation")
+	reply_service: ReplyService = ReplyService()
+	await reply_service.invoke()
