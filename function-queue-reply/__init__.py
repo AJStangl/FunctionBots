2b5cb33b15dd8fcd964a0f8f7e6ca63757f6134a
AJStangl <ajstangl@gmail.com>
2022-07-14 21:27:11 -0400

    Fixed more concurrency issues

--- function-queue-reply/__init__.py
+++ function-queue-reply/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 import azure.functions as func
@@ -8,5 +9,15 @@ from shared_code.services.reply_service import ReplyService
 async def main(message: func.QueueMessage) -> None:
 	logging.info(f":: Reply Service Invocation")
 	reply_service: ReplyService = ReplyService()
-	await reply_service.handle_message(message)
+	eventloop = asyncio.get_event_loop()
+	tasks = [
+		asyncio.create_task(
+			await eventloop.run_in_executor(
+				None,
+				reply_service.handle_message,
+				message
+			)
+		)
+	]
+	await asyncio.wait(tasks)
 	return None
--- function-queue-submission-worker/__init__.py
+++ function-queue-submission-worker/__init__.py
@@ -13,5 +13,15 @@ async def main(message: func.QueueMessage) -> None:
 	message_json = TableHelper.handle_message_generic(message)
 	bot_config = BotConfiguration(Name=message_json["Name"], Model=message_json["Model"], SubReddits=[message_json["SubReddit"]])
 	submission_service: SubmissionService = SubmissionService()
-	await submission_service.invoke(bot_config)
+	eventloop = asyncio.get_event_loop()
+	tasks = [
+		asyncio.create_task(
+			await eventloop.run_in_executor(
+				None,
+				submission_service.invoke,
+				bot_config
+			)
+		)
+	]
+	await asyncio.wait(tasks)
 	return None
